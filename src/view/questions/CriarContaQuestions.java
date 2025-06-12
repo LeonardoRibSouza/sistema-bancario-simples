ddbc2861a9a4412992c22bf0f69df7b534896c99
Leonardo <leonardoexemplo12@gmail.com>
2025-06-12 17:19:19 -0300

    arrumei o problema que tive no commit anterior

--- src/view/questions/CriarContaQuestions.java
+++ src/view/questions/CriarContaQuestions.java
@@ -2,39 +2,40 @@ package view.questions;
 
 import controller.BancoController;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CriarContaQuestions {
-   public static void interfaceCriarConta(BancoController banco) {
-       Scanner input = new Scanner(System.in);
+       public static void interfaceCriarConta(BancoController banco) throws IOException {
+              Scanner input = new Scanner(System.in);
 
-       System.out.println("---------------Criar-Conta------------");
-       System.out.println("Nome completo: ");
-       String nome = input.nextLine();
+              System.out.println("---------------Criar-Conta------------");
+              System.out.println("Nome completo: ");
+              String nome = input.nextLine();
 
-       System.out.println("Senha: ");
-       String senha = input.nextLine();
+              System.out.println("Senha: ");
+              String senha = input.nextLine();
 
-       System.out.println("Idade: ");
-       int idade = input.nextInt();
-       input.nextLine();
-       System.out.println("CPF: ");
-       String cpf = input.nextLine();
-
-
-       System.out.println("CEP: ");
-       String cep = input.nextLine();
+              System.out.println("Idade: ");
+              int idade = input.nextInt();
+              input.nextLine();
+              System.out.println("CPF: ");
+              String cpf = input.nextLine();
 
 
-       System.out.println("Telefone: ");
-       String telefone = input.nextLine();
+              System.out.println("CEP: ");
+              String cep = input.nextLine();
 
 
-       System.out.println("Email: ");
-       String email = input.nextLine();
+              System.out.println("Telefone: ");
+              String telefone = input.nextLine();
 
 
-       banco.cadastrarConta(nome,senha,idade,cpf,cep,telefone,email,banco);
+              System.out.println("Email: ");
+              String email = input.nextLine();
 
-   }
+
+              banco.cadastrarConta(nome,senha,idade,cpf,cep,telefone,email,banco);
+
+       }
 }
--- src/view/questions/LogarQuestions.java
+++ src/view/questions/LogarQuestions.java
@@ -2,11 +2,12 @@ package view.questions;
 
 import controller.BancoController;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LogarQuestions {
 
-    public static void login(BancoController banco){
+    public static void login(BancoController banco) throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
 
         System.out.println("""
